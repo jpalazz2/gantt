37fde86f4190c95295503b38eede927ca220bcf8
Joseph Palazzolo <Joseph Palazzolo>
2017-01-31 22:38:09 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @flow
+ */
+
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import GanttApp from 'UI/GanttApp';
+
+ReactDOM.render(<GanttApp />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname + '/build/',
+    publicPath: '/build/',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+      CSS: path.resolve(__dirname, 'lib/css/'),
+      UI: path.resolve(__dirname, 'lib/js/ui')
+    },
+    modules: [
+      path.resolve('./lib/js'),
+      path.resolve('./lib/css'),
+      path.resolve('./node_modules')
+    ]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-flow-strip-types']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+};
